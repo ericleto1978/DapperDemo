ad86a8dde0f7473c048f1308b6bd77b22eeed693
ericleto1978 <75315917+ericleto1978@users.noreply.github.com>
2022-07-28 10:16:27 -0400

    Add files via upload

---
+++ src/DapperDemo.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using DapperDemo.Infrastructure.Repository;
+using DapperDemo.Noyau.Interfaces;
+
+namespace DapperDemo.Infrastructure
+{
+    public static class ServiceRegistration
+    {
+        public static void InjecterInfrastructure(this IServiceCollection services)
+        {
+            services.AddTransient<IEntrepriseRepository, EntrepriseRepository>();
+        }
+    }
+}

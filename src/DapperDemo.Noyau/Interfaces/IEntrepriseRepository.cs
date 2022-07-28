8a956736a09338582153fec6a0173bc8d3ffb196
ericleto1978 <75315917+ericleto1978@users.noreply.github.com>
2022-07-28 10:19:53 -0400

    Add files via upload

---
+++ src/DapperDemo.Noyau/Interfaces/IEntrepriseRepository.cs
@@ -0,0 +1,9 @@
+﻿using DapperDemo.Noyau.Aggregats.Entreprise;
+
+namespace DapperDemo.Noyau.Interfaces
+{
+    public interface IEntrepriseRepository
+    {
+        public List<Entreprise> ObtenirLesEntreprises();
+    }
+}

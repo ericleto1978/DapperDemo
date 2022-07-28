7b50847610f56d9f51607380946f1488524f0669
ericleto1978 <75315917+ericleto1978@users.noreply.github.com>
2022-07-28 10:24:30 -0400

    Add files via upload

---
+++ src/CleanArchitecture/Commun/EntiteBase.cs
@@ -0,0 +1,12 @@
+﻿namespace CleanArchitecture.Commun
+{
+    public abstract class EntiteBase
+    {
+        public int Id  { get; set; } 
+
+        public EntiteBase()
+        {
+            Id = Guid.NewGuid().GetHashCode();
+        }
+    }
+}

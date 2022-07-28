0a4474011c7ca1d74987f78a074fb6b3edf442cb
ericleto1978 <75315917+ericleto1978@users.noreply.github.com>
2022-07-28 10:21:30 -0400

    Add files via upload

---
+++ src/DapperDemo.Web/Controllers/EntrepriseController.cs
@@ -0,0 +1,27 @@
+﻿using DapperDemo.Noyau.Aggregats.Entreprise;
+using DapperDemo.Noyau.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperDemo.Web.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]/[action]")]
+
+
+    public class EntrepriseController : ControllerBase
+    {
+        private readonly IEntrepriseRepository entrepriseRepo;
+
+        public EntrepriseController(IEntrepriseRepository repository)
+        {
+            entrepriseRepo = repository;
+        }
+
+        [HttpGet]
+        [ActionName("ObtenirLesEntreprises")]
+        public List<Entreprise> ObtenirLesEntreprises()
+        {
+            return entrepriseRepo.ObtenirLesEntreprises();
+        }
+    }
+}

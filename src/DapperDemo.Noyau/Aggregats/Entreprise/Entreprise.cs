b3ed67fd7fa095555ea26e2e31280033f270c266
ericleto1978 <75315917+ericleto1978@users.noreply.github.com>
2022-07-28 10:17:43 -0400

    Add files via upload

---
+++ src/DapperDemo.Noyau/Aggregats/Entreprise/Entreprise.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchitecture.Commun.Interfaces;
+using CleanArchitecture.Commun;
+
+
+namespace DapperDemo.Noyau.Aggregats.Entreprise
+{
+    public class Entreprise : EntiteBase, IAggregateRacine
+    {
+        public string Nom { get; set; }
+        public int NIM { get; set; }
+
+        public List<Participant> Participants { get; set; }
+
+        public Entreprise()
+        {
+            Participants = new List<Participant>();
+        }
+        public Entreprise(string nom, int nim)
+        {
+            Nom = nom;
+            NIM = nim;
+            Participants = new List<Participant>();
+        }
+    }
+}
---
+++ src/DapperDemo.Noyau/Aggregats/Entreprise/Participant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Commun;
+
+namespace DapperDemo.Noyau.Aggregats.Entreprise
+{
+    public class Participant : EntiteBase
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public int PourcentageParticipation { get; set; }
+
+
+        public Participant()
+        {
+
+        }
+        public Participant(string nom, string prenom, int pourcentage)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            PourcentageParticipation = pourcentage;
+        }
+    }
+}
